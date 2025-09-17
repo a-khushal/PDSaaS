1f22cddb87b875b165092e13ffc069b761db4343
Khushal <khushal2004a@gmail.com>
2025-09-17 23:19:31 +0530

    feat: bloom filter

--- core/bloom.go
+++ core/bloom.go
@@ -1,18 +1,75 @@
-package main
+package bloom
 
 import (
-	"fmt"
 	"math"
+
+	city "github.com/go-faster/city"
+	"github.com/spaolacci/murmur3"
 )
 
-const n = 1_000_000 // number of items in the filter
-const p = 0.001     // false positive porbability that we allow
+const n = 1_000_000 // expected items
+const p = 0.001     // false positive probability
 
-func main() {
-	m := int(math.Ceil(-float64(n) * math.Log(p) / math.Pow(math.Log(2), 2))) // the size of the bit array
-	k := int(math.Ceil((float64(m) / float64(n)) * math.Log(2)))              // Optimal number of hash functions
+type Bloom struct {
+	m      int
+	k      int
+	bitset []bool
+}
 
-	filter := make([]int, m)
+// creates a Bloom filter.
+func NewBloom() *Bloom {
+	m := int(math.Ceil(-float64(n) * math.Log(p) / math.Pow(math.Log(2), 2)))
+	k := int(math.Ceil((float64(m) / float64(n)) * math.Log(2)))
+	if k < 1 {
+		k = 1
+	}
+	return &Bloom{
+		m:      m,
+		k:      k,
+		bitset: make([]bool, m),
+	}
+}
 
-	fmt.Println(filter)
+func (bf *Bloom) hashes(data []byte) []int {
+	if bf.m == 0 || bf.k == 0 {
+		return nil
+	}
+
+	hashes := make([]int, 0, bf.k)
+
+	// base hashes both are in range [0, m-1]
+	h1 := int(murmur3.Sum32(data)) % bf.m
+	h2 := int(city.Hash64(data) % uint64(bf.m))
+
+	// ensure non-negative (Sum32 -> uint32 cast to int is non-negative, but safe-guard anyway)
+	if h1 < 0 {
+		h1 += bf.m
+	}
+	if h2 < 0 {
+		h2 += bf.m
+	}
+	// If h2 is 0, using (h1 + i*h2) will produce duplicates; make sure h2 != 0
+	if h2 == 0 {
+		h2 = 1
+	}
+	for i := 0; i < bf.k; i++ {
+		idx := (h1 + i*h2) % bf.m
+		hashes = append(hashes, idx)
+	}
+	return hashes
+}
+
+func (bf *Bloom) Add(item string) {
+	for _, idx := range bf.hashes([]byte(item)) {
+		bf.bitset[idx] = true
+	}
+}
+
+func (bf *Bloom) Contains(item string) bool {
+	for _, idx := range bf.hashes([]byte(item)) {
+		if !bf.bitset[idx] {
+			return false
+		}
+	}
+	return true
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/a-khushal/PDSaaS
 
 go 1.25.1
+
+require (
+	github.com/go-faster/city v1.0.1
+	github.com/spaolacci/murmur3 v1.1.0
+)
